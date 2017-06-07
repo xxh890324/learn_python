dd1cf6dc7540670ed64685262b6e709a1def5872
xuxianghao-win10 <402719462@qq.com>
2017-06-07 22:43:37 +0800

    frencdeck_test

--- fluent_python/card.py
+++ fluent_python/card.py
@@ -1,13 +1,15 @@
-#coding:utf-8
+# coding:utf-8
 import collections
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
-class FrenchDeck(object):
+
+class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        self._cards = [Card(rank, suit)
+                       for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
@@ -15,3 +17,7 @@ class FrenchDeck(object):
     def __getitem__(self, position):
         return self._cards[position]
 
+
+deck = FrenchDeck()
+for card in deck:
+    print card
